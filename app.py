7e8fc9214cdabdb487570c0812b31538eec7e45c
camillegff <camilleg1109@gmail.com>
2024-11-14 16:34:21 -0300

    adição das funções get para buscar título e gênero

--- app.py
+++ app.py
@@ -45,6 +45,11 @@ def submit_formulario():
     # Redirecionar para página de sucesso ou outra página
     return redirect(url_for('sucesso'))
 
+@app.route('/sucesso')
+def sucesso():
+    return 'Dados inseridos com sucesso!'
+
+
 
 @app.route('/livro/<int:livro_id>', methods=['PUT'])
 def atualizar_livro(livro_id):
@@ -69,16 +74,122 @@ def atualizar_livro(livro_id):
             return jsonify({'mensagem': 'Usuário atualizado com sucesso'}), 200
     except mysql.connector.Error as error:
         return jsonify({'error': str(error)}), 500
+    
+
+@app.route('/livros', methods=['GET'])
+def listar_livros():
+    try:
+        # Conectar ao banco de dados
+        with mysql.connector.connect(
+            host=db_host, user=db_user, password=db_password, database=db_name
+        ) as mydb:
+            mycursor = mydb.cursor()
+
+            # Executar a consulta SQL para selecionar todos os usuários
+            sql = "SELECT * FROM livros"
+            mycursor.execute(sql)
+
+            # Obter os resultados da consulta
+            resultados = mycursor.fetchall()
+
+            # Formatar os resultados em um JSON
+            livros = []
+            for livro in resultados:
+                livros.append({'id': livro[0], 'titulo': livro[1], 'genero': livro[2], 'sinopse': livro[3]})
+
+            return jsonify({'livros': livros}), 200
+    except mysql.connector.Error as error:
+        return jsonify({'error': str(error)}), 500
+
+@app.route('/get_livro/<int:livro_id>', methods=['GET'])
+def get_livro(livro_id):
+
+    try:
+        # Conectar ao banco de dados
+        with mysql.connector.connect(
+            host=db_host, user=db_user, password=db_password, database=db_name
+        ) as mydb:
+            mycursor = mydb.cursor()
+
+            # Executar a consulta SQL para selecionar todos os usuários
+            sql = "SELECT * FROM livros where id=%s "
+            valores = (livro_id,)
+
+            mycursor.execute(sql,valores)
+
+            # Obter os resultados da consulta
+            resultados = mycursor.fetchall()
+
+            # Formatar os resultados em um JSON
+            livros = []
+            for livro in resultados:
+                livros.append({'id': livro[0], 'titulo': livro[1], 'genero': livro[2], 'sinopse': livro[3]})
+
+            return jsonify({'livro': livros[0]}), 200
+    except mysql.connector.Error as error:
+        return jsonify({'error': str(error)}), 500
+    
+@app.route('/get_titulo_livro', methods=['GET'])
+def get_titulo_livro():
+    titulo = request.args.get('titulo')
+
+    if titulo:
+        # Construir a consulta SQL com o parâmetro de pesquisa
+        sql = "SELECT * FROM livros WHERE titulo LIKE %s"
+        valores = ('%' + titulo + '%',)
+        try:
+            with mysql.connector.connect(
+                host=db_host, user=db_user, password=db_password, database=db_name
+            ) as mydb:
+                mycursor = mydb.cursor()
+                mycursor.execute(sql, valores)
+                resultados = mycursor.fetchall()
+
+                # Formatar os resultados em um JSON
+                livros = []
+                for livro in resultados:
+                    livros.append({'id': livro[0], 'titulo': livro[1], 'genero': livro[2], 'sinopse': livro[3]})
+
+                return jsonify({'livros': livros}), 200
+        except mysql.connector.Error as error:
+            return jsonify({'error': str(error)}), 500
+    else:
+        return jsonify({'mensagem': 'O parâmetro "titulo" é obrigatório'}), 400
+    
+@app.route('/get_genero_livro', methods=['GET'])
+def get_genero_livro():
+    genero = request.args.get('genero')
+
+    if genero:
+        # Construir a consulta SQL com o parâmetro de pesquisa
+        sql = "SELECT * FROM livros WHERE genero LIKE %s"
+        valores = ('%' + genero + '%',)
+        try:
+            with mysql.connector.connect(
+                host=db_host, user=db_user, password=db_password, database=db_name
+            ) as mydb:
+                mycursor = mydb.cursor()
+                mycursor.execute(sql, valores)
+                resultados = mycursor.fetchall()
+
+                # Formatar os resultados em um JSON
+                livros = []
+                for livro in resultados:
+                    livros.append({'id': livro[0], 'titulo': livro[1], 'genero': livro[2], 'sinopse': livro[3]})
+
+                return jsonify({'livros': livros}), 200
+        except mysql.connector.Error as error:
+            return jsonify({'error': str(error)}), 500
+    else:
+        return jsonify({'mensagem': 'O parâmetro "titulo" é obrigatório'}), 400
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-# Rota para página de sucesso
-@app.route('/sucesso')
-def sucesso():
-    return 'Dados inseridos com sucesso!'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
 
 
